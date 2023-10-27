2a49fb614393dcdd6c114cf42f7d189f03b01e48
hbauhvagner <hbauhvagner@gmail.com>
2023-10-27 00:26:22 -0300

    Estrutura de dados Lista

---
+++ EstruturaDeDados.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+    
+    public static void main(String[] args) {
+        
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Vagner");
+        nomes.add("Joana");
+        nomes.add("João");
+
+        System.out.println(nomes.get(0));
+        for (String nome : nomes) {
+            System.out.println("O nome é: " + nome);
+        }
+
+        nomes.forEach(nome -> System.out.println("O nome que apareceu foi " + nome));
+    }
+}
