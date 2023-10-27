8b101495239e5fd63828dbc3efa7cf19161bfdcc
hbauhvagner <hbauhvagner@gmail.com>
2023-10-26 23:22:27 -0300

    Tipo de dados

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     
     public static void main(String[] args) {
-        
+
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 78945613L;
+        String dadoDoTipoString = "Colocar o meu texto";
+        boolean dadoDoTipoBoolean = true;
     }
 }
