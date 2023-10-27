c9563002bb5d6e16ce7235f426477596a4a004e0
hbauhvagner <hbauhvagner@gmail.com>
2023-10-26 23:41:28 -0300

    Estrutura de repetição

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,27 @@ public class HelloWorld {
         long dadoDoTipoLong = 78945613L;
         String dadoDoTipoString = "Colocar o meu texto";
         boolean dadoDoTipoBoolean = true;
+
+        if (dadoDoTipoInt == 10) {
+            System.out.println("Entrou no if");
+        } else if (dadoDoTipoInt == 12) {  
+            System.out.println("Entrou no if 12");
+        } else {
+            System.out.println("Entrou no else");
+        }
+
+        int valorInicial = 0;
+        while (valorInicial < 3) {
+            System.out.println("O valor inicial é menor que 3");
+            System.out.println(valorInicial);
+            valorInicial++;
+        }
+        System.out.println("Saiu do while");
+
+        System.out.println("Iniciando o FOR");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("O valor do i é: " + i);
+        }
+        System.out.println("Finalizando o FOR");
     }
 }
