3893f5fabb0e7ad8cc9f2f095b724f38074a36e6
hbauhvagner <hbauhvagner@gmail.com>
2023-10-27 22:57:56 -0300

    Hash Map

---
+++ Colecoes.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Colecoes {
+    
+    public static void main(String[] args) {
+        Map<String, Integer> notas = new HashMap<>();
+        notas.put("Vagner", 10);
+        notas.put("Joana", 10);
+        notas.put("João", 10);
+
+        var nota = notas.get("Vagner");
+        System.out.println(nota);
+
+        for (Map.Entry<String, Integer> entry: notas.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+            System.out.println("A chave é " + key + " e o valor é " + value);
+        }
+    }
+}
