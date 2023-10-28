db9e8df97f4e0bf2827d52da6800020aeab1bafc
hbauhvagner <hbauhvagner@gmail.com>
2023-10-28 00:44:07 -0300

    Polimorfismo

--- pessoa/Professor.java
+++ pessoa/Professor.java
@@ -11,4 +11,9 @@ public class Professor extends Pessoa {
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
+    String imprimirDadosDePessoa() {
+        System.out.println(super.imprimirDadosDaPessoa());
+        return "Você é professor";
+    }
 }
